0bfa797bf7534059a23277247deac3ee81e251ac
Monika Patel <moni@Monikas-MacBook-Air.local>
2022-03-18 19:50:14 -0400

    First commit

---
+++ src/api/server.ts
@@ -0,0 +1,55 @@
+let token = 'd89e6a8346f2b0d5c724328e4ccda0e92ddb7602d2271298'
+
+export const server_calls = {
+    get: async () => {
+        const response = await fetch(`https://myphonebookcodingtemple.herokuapp.com/api/contacts`,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': `Bearer ${token}`
+            }
+        });
+
+        if (!response.ok){
+            throw new Error('Failed to fetch data from server')
+        }
+
+        return await response.json()
+    },
+
+    create: async(data: any = {}) => {
+        const response = await fetch(`https://myphonebookcodingtemple.herokuapp.com/api/contacts`,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': `Bearer ${token}`
+            },
+            body: JSON.stringify(data)
+        });
+
+        if(!response.ok){
+            throw new Error('Failed to Create new data on server')
+        }
+
+        return await response.json()
+    },
+    update: async (id:string, data:any = {}) => {
+        const response = await fetch(`https://myphonebookcodingtemple.herokuapp.com/api/contacts/${id}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': `Bearer ${token}`
+            },
+            body: JSON.stringify(data)
+        });
+    },
+    delete: async(id:string) => {
+        const response = await fetch(`https://myphonebookcodingtemple.herokuapp.com//api/contacts/${id}`,{
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': `Bearer ${token}`
+            }
+        })
+    }
+}
---
+++ src/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyDhw7E8yRqkP6_BkOa4VuJX6PtNK7fqq-o",
+  authDomain: "react-phonebook-mp.firebaseapp.com",
+  projectId: "react-phonebook-mp",
+  storageBucket: "react-phonebook-mp.appspot.com",
+  messagingSenderId: "224075185323",
+  appId: "1:224075185323:web:e44ef6b60eb3ba824e8bee"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
